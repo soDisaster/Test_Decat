739f874c432cee48774bf0e115a4b5c43fca27d0
Anne-Sophie Saint-Omer <anne.sophie.saint.omer28@gmail.com>
2016-08-05 13:04:05 +0200

    Création des layouts et classes associées pour créer un compte.

---
+++ app/src/main/java/com/annesophie/test_decat/CreateAccountActivity.java
@@ -0,0 +1,31 @@
+package com.annesophie.test_decat;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    private Button buttonNext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+
+        // Create account
+
+        buttonNext = (Button) findViewById(R.id.buttonNext);
+
+    }
+
+    public void goToNext(View v){
+
+        Intent intentNext = new Intent(this, PreferDecathlonStoreActivity.class);
+        startActivity(intentNext);
+    }
+
+}
--- app/src/main/java/com/annesophie/test_decat/LoginActivity.java
+++ app/src/main/java/com/annesophie/test_decat/LoginActivity.java
@@ -1,13 +1,43 @@
 package com.annesophie.test_decat;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements OnClickListener{
+
+    private Button buttonCreateAccount, buttonLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Create account
+
+        buttonCreateAccount = (Button)findViewById(R.id.buttonCreateAccount);
+        buttonCreateAccount.setOnClickListener(this);
+
     }
+
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.buttonCreateAccount: {
+                Intent intentCreateAccount = new Intent(this, CreateAccountActivity.class);
+                startActivity(intentCreateAccount);
+                break;
+            }
+            case  R.id.buttonLogin: {
+                // do something
+                break;
+            }
+
+        }
+
+    }
+
 }
