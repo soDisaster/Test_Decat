d9db58ee752ea95db24c7b79dae5060003d48e33
Anne-Sophie Saint-Omer <anne.sophie.saint.omer28@gmail.com>
2016-08-10 02:55:06 +0200

    Nom bouton Déconnexion à Logout

--- app/src/main/java/com/annesophie/test_decat/CreateAccountActivity.java
+++ app/src/main/java/com/annesophie/test_decat/CreateAccountActivity.java
@@ -3,8 +3,10 @@ package com.annesophie.test_decat;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 /* Classe permettant à l'utilisateur d'entrer les informations le concernant.
    Etape 1/2
@@ -31,6 +33,14 @@ public class CreateAccountActivity extends AppCompatActivity {
 
     }
 
+    public final static boolean isValidEmail(CharSequence target) {
+        if (TextUtils.isEmpty(target)) {
+            return false;
+        } else {
+            return android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
+        }
+    }
+
     public void goToNext(View v){
 
         /*
@@ -46,17 +56,41 @@ public class CreateAccountActivity extends AppCompatActivity {
         password = editTextPassword.getText().toString();
         confirmPassword = editTextConfirmPassword.getText().toString();
 
-        /* Intent vers PreferDecathlonStoreActivity */
+        if(firstname.equals("") || lastname.equals("")
+                || email.equals("") || password.equals("") || confirmPassword.equals("") || firstname.equals(" ") || lastname.equals(" ")
+                || email.equals(" ") || password.equals(" ") || confirmPassword.equals(" ") || firstname.isEmpty() || lastname.isEmpty()
+                || email.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
 
-        Intent intentNext = new Intent(this, PreferDecathlonStoreActivity.class);
-        Bundle extras = new Bundle();
-        extras.putString("EXTRA_FIRSTNAME", firstname);
-        extras.putString("EXTRA_LASTNAME", lastname);
-        extras.putString("EXTRA_EMAIL", email);
-        extras.putString("EXTRA_PASSWORD",password);
-        extras.putString("EXTRA_CONFIRM_PASSWORD", confirmPassword);
-        intentNext.putExtras(extras);
-        startActivity(intentNext);
+            Toast.makeText(this, "Please complete text fields which are empty.", Toast.LENGTH_LONG).show();
+
+        }
+        else if(!(isValidEmail(email))){
+
+            Toast.makeText(this, "The email address is not correct", Toast.LENGTH_LONG).show();
+        }
+        else if(!(password.equals(confirmPassword))){
+
+            Toast.makeText(this, "The two passwords don't match", Toast.LENGTH_LONG).show();
+
+        }
+        else if(password.length() < 6){
+
+            Toast.makeText(this, "This password is too short", Toast.LENGTH_LONG).show();
+        }
+        else {
+
+            /* Intent vers PreferDecathlonStoreActivity */
+
+            Intent intentNext = new Intent(this, PreferDecathlonStoreActivity.class);
+            Bundle extras = new Bundle();
+            extras.putString("EXTRA_FIRSTNAME", firstname);
+            extras.putString("EXTRA_LASTNAME", lastname);
+            extras.putString("EXTRA_EMAIL", email);
+            extras.putString("EXTRA_PASSWORD", password);
+            extras.putString("EXTRA_CONFIRM_PASSWORD", confirmPassword);
+            intentNext.putExtras(extras);
+            startActivity(intentNext);
+        }
 
     }
 }
